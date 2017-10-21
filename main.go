a2d4fd8aca699e723b19b646ae7cba3b8a77c488
Florian Kaiser <florian.kaiser@fnkr.net>
2017-10-21 19:27:29 +0200

    Ability to customize host, port and verbosity

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
     "github.com/elazarl/goproxy"
     "log"
+    "fmt"
     "net/http"
 )
 
 func main() {
+    config := getConfig()
     proxy := goproxy.NewProxyHttpServer()
-    proxy.Verbose = true
-    log.Fatal(http.ListenAndServe(":9090", proxy))
+    proxy.Verbose = config.Verbose
+    log.Print(fmt.Sprintf("Listening on %s:%d...", config.Host, config.Port))
+    log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", config.Host, config.Port), proxy))
 }
